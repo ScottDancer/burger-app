c22ee596fca42dd5261f5d5f4c352e76de086ba2
Scott Dancer <scottpdancer@gmail.com>
2020-10-19 21:27:08 -0700

    burger app

---
+++ config/connection.js
@@ -0,0 +1,22 @@
+// Set up MySQL connection.
+var mysql = require("mysql");
+require("dotenv").config()
+var connection = mysql.createConnection(process.env.JAWSDB_URL||{
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: process.env.PASSWORD,
+  database: "burger_db"
+});
+
+// Make connection.
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+// Export connection for our ORM to use.
+module.exports = connection;
---
+++ db/seeds.sql
@@ -0,0 +1,2 @@
+INSERT INTO burgers (name,devoured) VALUES ('New Bacon-ings',false);
+INSERT INTO burgers (name,devoured) VALUES ('Rest in Peas',true);
